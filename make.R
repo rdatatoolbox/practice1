8f0ee8b34468269fb3fafc919d78ee18ca5aebad
Nicolas Casajus <nicolas.casajus@gmail.com>
2022-11-16 21:13:31 +0100

    Add makefile + Update deps

---
+++ make.R
@@ -0,0 +1,21 @@
+#' Practice 1 - Research Compendium
+#' 
+#' @author Nicolas Casajus \email{nicolas.casajus@fondationbiodiversite.fr}
+#' 
+#' @date 2022/11/16
+
+
+
+## Install Dependencies (listed in DESCRIPTION) ----
+
+remotes::install_deps(upgrade = "never")
+
+
+## Load Project Addins (R Functions and Packages) ----
+
+pkgload::load_all()
+
+
+## Run Project ----
+
+# source(here::here("analyses", "script_X.R"))
